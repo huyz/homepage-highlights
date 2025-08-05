9422843851146d06f956726213f89d8c784020d6
huyz <huyz@users.noreply.github.com>
2025-08-05 04:43:32 -0700

    @huyz's version for more flexibility

---
+++ README.md
@@ -0,0 +1,10 @@
+# homepage-highlights
+
+This custom JavaScript (and CSS) highlights cells in Homepage widgets based on thresholds/matches for better visibility of problems or action items.
+
+**Features:**
+- Customizable thresholds for each cell of each widget based on numerical comparison, string comparison, or regular expression.
+- Different alert styles (e.g., danger, warning, info)
+- Supports multiple instances of the same service
+
+See [[Feature Request] Improved visibility on service issues by highlighting fields. · gethomepage/homepage · Discussion #4619](https://github.com/gethomepage/homepage/discussions/4619).
--- custom.js
+++ custom.js
@@ -1,99 +1,196 @@
-/**
- * 2025-07-13 Second version by @CV8R at https://github.com/gethomepage/homepage/discussions/4619#discussioncomment-13747561
- */
+const DEFAULT_CLASSNAME = "alert-danger";
 
-function WatchItem(service, container, threshold) {
-    this.service = service;
-    this.container = container;
+const WATCH_ITEMS = [
+  //// Generally errors
 
-    // Parse operator and value from the threshold string, e.g., ">0"
-    const match = threshold.match(/(==|!=|>=|<=|>|<)(.+)/);
-    if (!match) throw new Error(`Invalid threshold format: ${threshold}`);
+  new WatchItem('UptimeRobot', 0, "!=Up"),
+  new WatchItem('Healthchecks.io', 1, ">0"),
+  new WatchItem('Healthchecks', 1, ">0"),
 
-    this.operator = match[1];
-    this.threshold = match[2].trim();
-}
+  new WatchItem('Prometheus', 1, ">0"),
+  new WatchItem('Grafana', 2, ">0"),
+  new WatchItem('Grafana', 3, ">0"),
 
-const watchItems = [
-    new WatchItem('UPS', 0, "!=On-Line"),
-    new WatchItem('UPS', 1, ">80"),
-    new WatchItem('UPS', 2, "<100"),
-    new WatchItem('UPS', 3, ">40"),
-    new WatchItem('Proxmox PBS1', 0, ">80"),
-    new WatchItem('Proxmox PBS1', 1, ">0"),
-    new WatchItem('Proxmox PBS1', 2, ">80"),
-    new WatchItem('Proxmox PBS1', 3, ">80"),
-    new WatchItem('Proxmox Cluster', 2, ">80"),
-    new WatchItem('Proxmox Cluster', 3, ">80"),
-    new WatchItem('TrueNAS', 2, ">0"),
-    new WatchItem('BackupTrueNAS', 2, ">0"),
-    new WatchItem('Portainer Docker PVE VM1', 1, ">0"),
-    new WatchItem('Portainer Docker PVE VM2', 1, ">0"),
-    new WatchItem('Portainer Docker TN VM1', 1, ">0"),
-    new WatchItem('Portainer Docker TN VM2', 1, ">0"),
-    new WatchItem('Zabbix', 2, ">0"),
-    new WatchItem('Zabbix', 3, ">0"),
+  new WatchItem('Caddy', 2, ">0"),
+  new WatchItem('Portainer', 1, ">0"),
+  // 2025-08-04 Crowdsec widget is broken
+  // https://github.com/gethomepage/homepage/discussions/3472
+  //new WatchItem('Crowdsec', 1, ">0"),
+  //new WatchItem('Crowdsec', 0, ">0", {classname: 'alert-warning'}),
+  new WatchItem('Technitium', 1, ">0"),
+  new WatchItem('Uptime Kuma', 1, ">0"),
+  new WatchItem('Watchtower', 2, ">0"),
+
+  //// Generally warnings
+
+  // Cloudflare
+  new WatchItem('Cloudflare', 0, "!=Healthy", {
+    classname: 'alert-warning',
+    matchSelector: '[data-name^="CF:"]',
+    expectedMatches: 8,
+  }),
+
+  // Tailscale: "Now" and "…s Ago" are both ok, so exclude everything else using a regular expression
+  new WatchItem('Tailscale' , 1, "=~ ([mhdw]|mo) Ago", {
+    classname: 'alert-warning',
+    matchSelector: '[data-name^="TS:"]',
+    expectedMatches: 5,
+  }),
+
+  //// INFO: items to process
+
+  new WatchItem('Paperless', 0, ">0", {classname: 'alert-info'}),
+  new WatchItem('Changedetection', 0, ">0", {classname: 'alert-info'}),
+
+  //// Other examples
+
+  //new WatchItem('UPS', 0, "!=On-Line"),
+  //new WatchItem('UPS', 1, ">80"),
+  //new WatchItem('UPS', 2, "<100"),
+  //new WatchItem('UPS', 3, ">40"),
+  //new WatchItem('Proxmox PBS1', 0, ">80"),
+  //new WatchItem('Proxmox PBS1', 1, ">0"),
+  //new WatchItem('Proxmox PBS1', 2, ">80"),
+  //new WatchItem('Proxmox PBS1', 3, ">80"),
+  //new WatchItem('Proxmox Cluster', 2, ">80"),
+  //new WatchItem('Proxmox Cluster', 3, ">80"),
+  //new WatchItem('TrueNAS', 2, ">0"),
+  //new WatchItem('BackupTrueNAS', 2, ">0"),
+  //new WatchItem('Portainer Docker PVE VM1', 1, ">0"),
+  //new WatchItem('Portainer Docker PVE VM2', 1, ">0"),
+  //new WatchItem('Portainer Docker TN VM1', 1, ">0"),
+  //new WatchItem('Portainer Docker TN VM2', 1, ">0"),
+  //new WatchItem('Zabbix', 2, ">0"),
+  //new WatchItem('Zabbix', 3, ">0"),
+
+  //new WatchItem('Portainer', 1, ">0"),
+  //new WatchItem('Uptime Kuma', 1, ">0"),
+  //new WatchItem('Healthchecks.io', 1, ">0"),
+  //new WatchItem('Uptime Robot', 1, ">0"),
+  //new WatchItem('Crowdsec', 1, ">0"),
+  //new WatchItem('Omada', 2, ">0"),
+  //new WatchItem('NetAlertX', 3, ">0"),
+  //new WatchItem('Truenas', 2, ">0"),
+  //new WatchItem('Scrutiny', 1, ">0"),
+  //new WatchItem('Scrutiny', 2, ">0"),
 ];
 
+/**
+ *
+ * @param {*} options.matchSelector - By default, this app looks for a service that matches by name.
+ *    This option allows you to specify a custom selector, which is particularly useful
+ *    if you have several instances of widgets for the same service.
+ * @param {*} options.expectedMatches - How many matches to look for (and add observers to).
+ *    It's important to set this accurately to cover all intended widgets while avoiding
+ *    wasting CPU cycles on unnecessary observers.
+ */
+function WatchItem(service, cellIndex, threshold, options) {
+  this.service = service;
+  this.cellIndex = cellIndex;
+  this.classname = options?.classname ?? DEFAULT_CLASSNAME;
+  this.matchSelector = options?.matchSelector ?? `[data-name="${this.service}"]`;
+  this.expectedMatches = options?.expectedMatches ?? 1;
+
+  // Parse operator and value from the threshold string, e.g., ">0"
+  const match = threshold.match(/(==|!=|>=|<=|>|<|=~|!~)(.+)/);
+  if (!match) throw new Error(`Invalid threshold format: ${threshold}`);
+
+  this.operator = match[1];
+  this.threshold = match[2].trim();
+}
+
 function compareValues(current, threshold, operator) {
-    // Try to coerce to numbers; fallback to strings
-    const numCurrent = parseFloat(current);
-    const numThreshold = parseFloat(threshold);
+  // Try to coerce to numbers; fallback to strings
+  const numCurrent = parseFloat(current);
+  const numThreshold = parseFloat(threshold);
 
-    const useNumbers = !isNaN(numCurrent) && !isNaN(numThreshold);
+  const useNumbers = !isNaN(numCurrent) && !isNaN(numThreshold);
 
-    const a = useNumbers ? numCurrent : current;
-    const b = useNumbers ? numThreshold : threshold;
+  const a = useNumbers ? numCurrent : current;
+  const b = useNumbers ? numThreshold : threshold;
 
-    switch (operator) {
-        case "==": return a == b;
-        case "!=": return a != b;
-        case ">":  return a > b;
-        case "<":  return a < b;
-        case ">=": return a >= b;
-        case "<=": return a <= b;
-        default:
-            console.error(`Unsupported operator: ${operator}`);
-            return false;
-    }
+  switch (operator) {
+    case "==": return a == b;
+    case "!=": return a != b;
+    case ">":  return a > b;
+    case "<":  return a < b;
+    case ">=": return a >= b;
+    case "<=": return a <= b;
+    case "=~": return a.match(new RegExp(b));
+    case "!~": return !a.match(new RegExp(b));
+    default:
+    console.error(`Unsupported operator: ${operator}`);
+    return false;
+  }
 }
 
 async function observeItem(item) {
-    await waitForItem(item);
+  for (let i = 0; i < item.expectedMatches; ++i) {
+    // First, wait until the item is rendered in the page DOM
+    const cell = await waitForItem(item, i, i == item.expectedMatches - 1);
+    /* Debugging
+    if (i == 0) {
+    console.log(`${item.matchSelector}: ✅`);
+    } else {
+      console.log(`${item.matchSselector}: waiting for ${item.expectedMatches - i - 1} more`);
+    }
+    */
 
-    item.element = document
-        .querySelector(`[data-name='${item.service}']`)
-        .querySelector(".service-container")
-        .children[item.container];
+    // Now we can get at the appropriate cell
+    const element = cell.children[item.cellIndex];
 
     const observer = new MutationObserver(function (mutations) {
-        mutations.forEach(function (mutation) {
-            const currentValue = mutation.target.data;
+      mutations.forEach(function (mutation) {
+        const currentValue = mutation.target.data;
 
-            if (compareValues(currentValue, item.threshold, item.operator)) {
-                item.element.classList.add("fail-bg");
-            } else {
-                item.element.classList.remove("fail-bg");
-            }
+        element.classList.forEach(cls => {
+          if (cls.startsWith("alert-")) {
+            element.classList.remove(cls);
+          }
         });
+
+        if (compareValues(currentValue, item.threshold, item.operator)) {
+          element.classList.add(item.classname);
+        }
+
+        // Re-highlight the box that is being observed because Homepage has re-created it
+        element.classList.add('observed-for-alerts');
+      });
     });
 
-    observer.observe(item.element, { subtree: true, characterData: true });
+    // We only care about the first line in hte cell
+    const valueElement = element.children[0];
+    observer.observe(valueElement, { subtree: true, characterData: true });
+
+    // Highlight the boxes that are being observed
+    element.classList.add('observed-for-alerts');
+  }
 }
 
-function waitForItem(item) {
-    return new Promise(resolve => {
-        const observer = new MutationObserver(() => {
-            const container = document.querySelector(`[data-name='${item.service}']`)?.querySelector(".service-container");
-            if (container) {
-                observer.disconnect();
-                resolve();
-            }
-        });
-
-        observer.observe(document.body, { childList: true, subtree: true });
+/**
+* Wait for the item to be added to the page DOM, in order to add the real observer
+* on the cell's values.
+*/
+function waitForItem(item, matchNumber, disconnectWhenDone) {
+  return new Promise(resolve => {
+    const observer = new MutationObserver(() => {
+      const matches = document.querySelectorAll(item.matchSelector);
+      if (matches.length >= matchNumber + 1) {
+        const cell = matches[matchNumber]?.querySelector(".service-container");
+        // The cell is in the DOM, let's get this observer replaced.
+        if (cell) {
+          if (disconnectWhenDone) {
+            // This is important to avoid wasted CPU cycles
+            observer.disconnect();
+          }
+          resolve(cell);
+        }
+      }
     });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+  });
 }
 
 // Start observers
-watchItems.forEach(observeItem);
+WATCH_ITEMS.forEach(observeItem);
